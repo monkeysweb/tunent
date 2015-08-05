08f9bef33232c1aa9319e7a7b6c9b96c4c4dbf4b
harry1064 <Prince@Harpreets-Mac-mini.local>
2015-08-06 01:25:10 +0530

    error removed from like.js

--- models/like.js
+++ models/like.js
@@ -11,7 +11,7 @@ likeSchema.statics.addToLikeTable = function (data, callback) {
  		this.create(data,callback);
 }
 
-ikeSchema.statics.checkForDuplicateEntry= function (data, callback) {
+likeSchema.statics.checkForDuplicateEntry= function (data, callback) {
 	
  		this.findOne({artist_id:data.artist_id,track_id:data.track_id},callback);
 }
