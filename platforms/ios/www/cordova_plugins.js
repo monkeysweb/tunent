9e8e59439573bc0f6e23498ad1bdb64be53aa740
Haroon Dilshad <ronniekinsley@ronniekinsley-Inspiron-N5110.(none)>
2015-09-11 16:16:57 +0500

    Added the iOS platform in /platforms

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.oauthio.plugins.oauthio/www/dist/oauth.js",
+        "id": "com.oauthio.plugins.oauthio.OAuth",
+        "merges": [
+            "OAuth"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
+        "id": "org.apache.cordova.inappbrowser.inappbrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.oauthio.plugins.oauthio": "0.2.4",
+    "com.phonegap.plugins.facebookconnect": "0.11.0",
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.inappbrowser": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
